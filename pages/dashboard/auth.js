fa48d1efa02103406bdc3fddb7ede4984a1c5b84
Hasan <mr.k779@outlook.com>
2022-08-20 02:51:36 +0600

    Firebase login

--- pages/dashboard/auth.js
+++ pages/dashboard/auth.js
@@ -1,10 +1,33 @@
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "/lib/firebase/firebaseConfig";
+const provider = new GoogleAuthProvider();
+
 export default function Auth() {
   function handleClick() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user);
+        console.log(credential);
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
   }
 
   return (
     <div className="auth_container">
-      <button onClick={this.handleClick}>
+      <button onClick={handleClick}>
         Login With Google
       </button>
     </div>
