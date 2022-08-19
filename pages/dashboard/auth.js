a8079d199118ed0f908cd2bbc3b03a9eb22862f9
Hasan <mr.k779@outlook.com>
2022-08-19 23:17:00 +0600

    Create auth.js

---
+++ pages/dashboard/auth.js
@@ -0,0 +1,12 @@
+export default function Auth() {
+  function handleClick() {
+  }
+
+  return (
+    <div className="auth_container">
+      <button onClick={this.handleClick}>
+        Login With Google
+      </button>
+    </div>
+  )
+}
