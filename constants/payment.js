5172af8f9b8c5495f7b612ae1efaf7d04edc8c6d
Hasan <mr.k779@outlook.com>
2022-08-30 02:26:59 +0600

    Creating new checkout session

---
+++ constants/payment.js
@@ -0,0 +1,5 @@
+export const CURRENCY = 'usd';
+// Set your amount limits: Use float for decimal currencies and
+// Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
+export const MIN_AMOUNT = 5.0;
+export const MAX_AMOUNT = 500.0;
---
+++ pages/api/checkout_sessions/index.js
@@ -0,0 +1,41 @@
+import Stripe from 'stripe';
+import { CURRENCY, MIN_AMOUNT, MAX_AMOUNT } from '../../../constants/payment';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2020-08-27',
+});
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const amount = req.body.priceId;
+    try {
+      // Validate the amount that was passed from the client.
+      if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
+        throw new Error('Invalid amount.');
+      }
+      // Create Checkout Sessions from body params.
+      const params = {
+        mode: 'subscription',
+        line_items: [
+          {
+            price: priceId,
+            quantity: 1,
+          },
+        ],
+        success_url: `${req.headers.origin}/payment/result?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/payment/cancelled?session_id={CHECKOUT_SESSION_ID}`,
+      };
+      const checkoutSession = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(checkoutSession);
+    } catch (err) {
+      res.status(500).json({ statusCode: 500, message: err.message });
+    }
+  } else if (req.method === 'GET') {
+    const { sessionId } = req.query;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+    res.send(session);
+  } else {
+    res.status(405).end('Method Not Allowed');
+  }
+}
