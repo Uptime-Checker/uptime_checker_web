5b6804893b5cf68713ab1ba43a6afdb8e33766e2
Hasan <mr.k779@outlook.com>
2022-08-31 03:42:12 +0600

    Update payment.js

--- pages/dashboard/payment.js
+++ pages/dashboard/payment.js
@@ -1,9 +1,32 @@
+import axios from 'axios';
+import getStripe from '../../lib/stripe';
 import { basicClient } from '../../lib/axios';
 
-export default function Payment(products) {
+export default function Payment({ products }) {
   async function handleClick(e) {
     e.preventDefault();
-    console.log(products);
+
+    const response = await axios.post('/api/checkout_sessions', {
+      priceId: products[0].prices[0].id,
+    });
+
+    if (response.statusCode === 500) {
+      console.error(response.message);
+      return;
+    }
+
+    // Redirect to Checkout.
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
+      // Make the id field from the Checkout Session creation API response
+      // available to this file, so you can provide it as parameter here
+      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+      sessionId: response.id,
+    });
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    console.warn(error.message);
   }
 
   return (
