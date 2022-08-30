9f30f7625bb08f4cd6c64876917b6805eebc49fa
Hasan <mr.k779@outlook.com>
2022-08-31 04:01:51 +0600

    Update payment.js

--- pages/dashboard/payment.js
+++ pages/dashboard/payment.js
@@ -15,17 +15,10 @@ export default function Payment({ products }) {
       return;
     }
 
-    // Redirect to Checkout.
     const stripe = await getStripe();
     const { error } = await stripe.redirectToCheckout({
-      // Make the id field from the Checkout Session creation API response
-      // available to this file, so you can provide it as parameter here
-      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
       sessionId: response.data.id,
     });
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `error.message`.
     console.warn(error.message);
   }
 
@@ -40,9 +33,5 @@ export async function getServerSideProps() {
   const res = await elixirClient.get(`/external_products`);
   const products = res.data.data;
 
-  return {
-    props: {
-      products,
-    },
-  };
+  return { props: { products } };
 }
