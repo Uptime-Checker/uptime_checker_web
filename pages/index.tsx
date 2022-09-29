c488ceb3e92c0219849fe542be2c9f50fdd1fa85
Hasan <mr.k779@outlook.com>
2022-09-30 05:07:59 +0600

    Added tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Uptime Checker!</h1>
-
+        <h1 className="text-6xl font-bold">Welcome to Uptime Checker!</h1>
         <p className={styles.description}>Launching Soon....</p>
       </main>
 
