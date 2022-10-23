3f4b0d37d89972fe9a088d69c8f352eabf813f0f
Hasan <mr.k779@outlook.com>
2022-10-23 14:11:39 +0600

    Added hero icons

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -77,6 +77,7 @@ export default function Auth() {
                     id="email"
                     name="email"
                     type="email"
+                    placeholder="you@example.com"
                     autoComplete="email"
                     required
                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
