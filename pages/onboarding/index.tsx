0375366856e76cd9e483e6f749009202e489d232
Hasan <mr.k779@outlook.com>
2022-10-29 04:22:11 +0600

    Update index.tsx

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -1,8 +1,21 @@
+import LoadingIcon from 'components/icon/loading';
+import { FormEvent, useState } from 'react';
+
 export default function Onboarding() {
+  const [loading, setLoading] = useState(false);
+
+  const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+  };
+
   return (
     <div className="min-h-screen bg-gray-50">
       <div className="w-full py-32 sm:mx-auto sm:max-w-md sm:px-6 lg:px-8">
-        <form className="flex flex-col items-center justify-center space-y-6 sm:mx-auto sm:w-full sm:max-w-2xl">
+        <form
+          className="flex flex-col items-center justify-center space-y-6 sm:mx-auto sm:w-full sm:max-w-2xl"
+          onSubmit={handleFormSubmit}
+        >
           <h1 className="px-4 text-center text-4xl font-bold text-gray-900 sm:px-0">Let's get started</h1>
           <div className="w-full bg-white px-4 py-5 drop-shadow-md sm:rounded-lg sm:p-6">
             <div className="mt-5 md:col-span-2 md:mt-0">
@@ -14,6 +27,8 @@ export default function Onboarding() {
                     </label>
                     <div className="mt-1">
                       <input
+                        required
+                        disabled={loading}
                         type="text"
                         name="firstName"
                         id="firstName"
@@ -31,6 +46,8 @@ export default function Onboarding() {
                     </label>
                     <div className="mt-1">
                       <input
+                        required
+                        disabled={loading}
                         type="text"
                         name="lastName"
                         id="lastName"
@@ -48,6 +65,8 @@ export default function Onboarding() {
                     </label>
                     <div className="mt-1">
                       <input
+                        required
+                        disabled={loading}
                         type="text"
                         name="organisation"
                         id="organisation"
@@ -65,8 +84,10 @@ export default function Onboarding() {
           </div>
           <button
             type="submit"
+            disabled={loading}
             className="inline-flex items-center rounded-md border border-gray-300 bg-white px-10 py-3 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
           >
+            {loading ? <LoadingIcon className="-ml-1 mr-3 h-5 w-5 animate-spin text-indigo-700" /> : null}
             Continue
           </button>
         </form>
