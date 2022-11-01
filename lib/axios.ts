32bee0d9666033b2d45a9ccfdc497dd953e76a48
Hasan <mr.k779@outlook.com>
2022-11-01 22:25:46 +0600

    Update axios.ts

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,4 +1,16 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { getAccessToken } from './global';
+
+const UNAUTHENTICATED = 401;
+
+export enum HTTPMethod {
+  DELETE = 'DELETE',
+  GET = 'GET',
+  HEAD = 'HEAD',
+  PATCH = 'PATCH',
+  POST = 'POST',
+  PUT = 'PUT',
+}
 
 const client: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_ELIXIR_API,
@@ -8,6 +20,36 @@ const client: AxiosInstance = axios.create({
 
 export const addToken = (token: string) => {
   client.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+};
+
+export const authClientRequest = async <T = any, R = AxiosResponse<T>, D = any>(
+  config: AxiosRequestConfig<D>
+): Promise<R> => {
+  try {
+    const token = getAccessToken();
+    if (token === null) {
+      redirectToAuth();
+      return <R>await Promise.resolve();
+    } else {
+      addToken(token);
+    }
+    return await client.request<T, R, D>(config);
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      if (error.response.status === UNAUTHENTICATED) {
+        redirectToAuth();
+        return <R>await Promise.resolve();
+      } else {
+        throw error;
+      }
+    } else {
+      throw error;
+    }
+  }
+};
+
+const redirectToAuth = () => {
+  window.location.replace(`${window.location.origin}/auth`);
 };
 
 export const elixirClient = client;
