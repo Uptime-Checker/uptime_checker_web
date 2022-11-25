935f850a1ea1fd1d79abbfbb7c50779524124fd0
Hasan <mr.k779@outlook.com>
2022-11-25 21:46:38 +0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,27 @@ import type { AppProps } from 'next/app';
 import 'styles/globals.css';
 
 import '@tremor/react/dist/esm/tremor.css';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const layoutEnabledPage = ({ Component, pageProps }: AppPropsWithLayout) => {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(<Component {...pageProps} />);
+};
+
+function MyApp(appPropsWithLayout: AppPropsWithLayout) {
   return (
     <RootLayout>
-      <ErrorBoundary>
-        <Component {...pageProps} />
-      </ErrorBoundary>
+      <ErrorBoundary>{layoutEnabledPage(appPropsWithLayout)}</ErrorBoundary>
     </RootLayout>
   );
 }
