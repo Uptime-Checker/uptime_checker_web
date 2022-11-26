781658667fe9ce330ce36edad554fd1aee019ecc
Hasan <mr.k779@outlook.com>
2022-11-27 03:36:22 +0600

    Moved layout

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,44 +1,19 @@
-import { Bars3Icon } from '@heroicons/react/24/outline';
-import { useAtom } from 'jotai';
 import DashboardLayout from 'layout/dashboard-layout';
 import { ReactElement } from 'react';
-import { globalAtom } from 'store/global';
 import { NextPageWithLayout } from '../_app';
 
 const Dashboard: NextPageWithLayout = () => {
-  const [, setGlobal] = useAtom(globalAtom);
-  const toggleSidebar = () =>
-    setGlobal((draft) => {
-      draft.sidebar = !draft.sidebar;
-    });
-
   return (
     <>
-      <div className="flex flex-1 flex-col md:pl-64">
-        <div className="sticky top-0 z-10 bg-white pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
-          <button
-            type="button"
-            className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-            onClick={toggleSidebar}
-          >
-            <span className="sr-only">Open sidebar</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+        <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+      </div>
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+        {/* Replace with your content */}
+        <div className="py-4">
+          <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
         </div>
-        <main className="flex-1">
-          <div className="py-6">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-              <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-            </div>
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-              {/* Replace with your content */}
-              <div className="py-4">
-                <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-              </div>
-              {/* /End replace */}
-            </div>
-          </div>
-        </main>
+        {/* /End replace */}
       </div>
     </>
   );
