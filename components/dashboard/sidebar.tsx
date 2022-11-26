e13f962ed2813611e01f304e62b0e8d7ef8f79f3
Hasan <mr.k779@outlook.com>
2022-11-27 03:33:34 +0600

    Update sidebar.tsx

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -29,6 +29,16 @@ const SideBar = () => {
       draft.sidebar = !draft.sidebar;
     });
 
+  const logo = (
+    <div className="flex flex-shrink-0 items-center px-4">
+      <img
+        className="h-8 w-auto"
+        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+        alt="Your Company"
+      />
+    </div>
+  );
+
   return (
     <>
       <Transition.Root show={global.sidebar} as={Fragment}>
@@ -77,13 +87,7 @@ const SideBar = () => {
                   </div>
                 </Transition.Child>
                 <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
-                  <div className="flex flex-shrink-0 items-center px-4">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt="Your Company"
-                    />
-                  </div>
+                  {logo}
                   <nav className="mt-5 space-y-1 px-2">
                     {navigation.map((item) => (
                       <a
@@ -137,13 +141,7 @@ const SideBar = () => {
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
           <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
-            <div className="flex flex-shrink-0 items-center px-4">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-              />
-            </div>
+            {logo}
             <nav className="mt-5 flex-1 space-y-1 bg-white px-2">
               {navigation.map((item) => (
                 <a
