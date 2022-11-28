163400453f555048416d04b89c2e3aa9bce307c9
Hasan <mr.k779@outlook.com>
2022-11-29 03:40:55 +0600

    Logo without text

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -10,6 +10,7 @@ import {
   UsersIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline';
+import LogoWithoutText from 'components/logo/logo-without-text';
 import { useAtom } from 'jotai';
 import { Fragment } from 'react';
 import { globalAtom } from 'store/global';
@@ -33,11 +34,7 @@ const SideBar = () => {
 
   const logo = (
     <div className="flex flex-shrink-0 items-center px-4">
-      <img
-        className="h-8 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company"
-      />
+      <LogoWithoutText className="h-8 w-auto" />
     </div>
   );
 
