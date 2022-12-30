3a52d8d4d74756e0cf6fa2d5a86af409c72dca82
Hasan <mr.k779@outlook.com>
2022-12-31 03:38:14 +0600

    Update profile.tsx

--- pages/settings/profile.tsx
+++ pages/settings/profile.tsx
@@ -84,7 +84,7 @@ const Profile: NextPageWithLayout = () => {
         </div>
 
         <div className="sm:col-span-3">
-          <div className="flex justify-end">
+          <div className="flex min-h-full items-end justify-end">
             <button
               type="submit"
               className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
