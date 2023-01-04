8202e66c2dc31247e108d07a245bf2a3735ff10c
Hasan <mr.k779@outlook.com>
2023-01-04 23:17:41 +0600

    Update billing.tsx

--- pages/settings/billing.tsx
+++ pages/settings/billing.tsx
@@ -17,6 +17,15 @@ const pricing = {
       mostPopular: false,
     },
     {
+      title: 'Developer',
+      price: 28,
+      frequency: '/month',
+      description: 'The essentials to provide your best work for clients.',
+      features: ['15 products', 'Up to 1,500 subscribers', 'Basic analytics', '24-hour support response time'],
+      cta: 'Monthly billing',
+      mostPopular: false,
+    },
+    {
       title: 'Startup',
       price: 32,
       frequency: '/month',
@@ -35,7 +44,7 @@ const pricing = {
       title: 'Enterprise',
       price: 48,
       frequency: '/month',
-      description: 'Dedicated support and infrastructure for your company.',
+      description: 'Dedicated support and infrastructure for enterprise.',
       features: [
         'Unlimited products',
         'Unlimited subscribers',
@@ -52,20 +61,20 @@ const pricing = {
 
 const Billing: NextPageWithLayout = () => {
   return (
-    <div className="mx-auto max-w-7xl bg-white py-10 px-4 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl bg-white py-10">
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-2xl sm:leading-none lg:text-3xl">
         Pricing plans for teams of all sizes
       </h2>
       <p className="mt-3 max-w-2xl text-gray-500">All plans come with a 30-day money-back guarantee</p>
 
       {/* Tiers */}
-      <div className="mt-24 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-8 lg:space-y-0">
+      <div className="mt-12 space-y-4 sm:mt-16 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-4">
         {pricing.tiers.map((tier) => (
           <div
             key={tier.title}
-            className="relative flex flex-col rounded-2xl border border-gray-200 bg-white p-8 shadow-sm"
+            className="relative flex flex-col divide-y divide-gray-200 rounded-2xl border border-gray-200 bg-white shadow-sm"
           >
-            <div className="flex-1">
+            <div className="p-6">
               <h3 className="text-xl font-semibold text-gray-900">{tier.title}</h3>
               {tier.mostPopular ? (
                 <p className="absolute top-0 -translate-y-1/2 transform rounded-full bg-indigo-500 py-1.5 px-4 text-sm font-semibold text-white">
@@ -73,33 +82,36 @@ const Billing: NextPageWithLayout = () => {
                 </p>
               ) : null}
               <p className="mt-4 flex items-baseline text-gray-900">
-                <span className="text-5xl font-bold tracking-tight">${tier.price}</span>
+                <span className="text-4xl font-bold tracking-tight">${tier.price}</span>
                 <span className="ml-1 text-xl font-semibold">{tier.frequency}</span>
               </p>
               <p className="mt-6 text-gray-500">{tier.description}</p>
 
-              {/* Feature list */}
-              <ul role="list" className="mt-6 space-y-6">
+              <a
+                href="#"
+                className={classNames(
+                  tier.mostPopular
+                    ? 'bg-indigo-500 text-white hover:bg-indigo-600'
+                    : 'bg-indigo-50 text-indigo-700 hover:bg-indigo-100',
+                  'mt-8 block w-full rounded-md border border-transparent py-3 px-6 text-center font-medium'
+                )}
+              >
+                Upgrade
+              </a>
+            </div>
+
+            {/* Feature list */}
+            <div className="px-6 pt-6 pb-8">
+              <h3 className="text-sm font-medium text-gray-900">What&apos;s included</h3>
+              <ul role="list" className="mt-6 space-y-4">
                 {tier.features.map((feature) => (
-                  <li key={feature} className="flex">
-                    <CheckIcon className="h-6 w-6 flex-shrink-0 text-indigo-500" aria-hidden="true" />
-                    <span className="ml-3 text-gray-500">{feature}</span>
+                  <li key={feature} className="flex space-x-3">
+                    <CheckIcon className="h-5 w-5 flex-shrink-0 text-green-500" aria-hidden="true" />
+                    <span className="text-sm text-gray-500">{feature}</span>
                   </li>
                 ))}
               </ul>
             </div>
-
-            <a
-              href="#"
-              className={classNames(
-                tier.mostPopular
-                  ? 'bg-indigo-500 text-white hover:bg-indigo-600'
-                  : 'bg-indigo-50 text-indigo-700 hover:bg-indigo-100',
-                'mt-8 block w-full rounded-md border border-transparent py-3 px-6 text-center font-medium'
-              )}
-            >
-              {tier.cta}
-            </a>
           </div>
         ))}
       </div>
