fe034b29f557bcfe246b6367c9ba2d44550964bd
Hasan <mr.k779@outlook.com>
2023-01-04 23:20:30 +0600

    Update index.tsx

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -38,7 +38,7 @@ const Team: NextPageWithLayout = () => {
               <h1 className="text-xl font-semibold text-gray-900">Users</h1>
               <p className="mt-2 text-sm text-gray-700">A list of all the users in the team</p>
             </div>
-            <div className="mt-4 mt-0 ml-16 flex-none">
+            <div className="mt-4">
               <button
                 type="button"
                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
