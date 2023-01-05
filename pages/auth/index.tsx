ab0842e860456492ee51a289a6b0bc0308232638
Hasan <mr.k779@outlook.com>
2023-01-05 21:14:56 +0600

    Update index.tsx

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -174,6 +174,7 @@ export default function Auth() {
                     type="email"
                     placeholder="you@example.com"
                     autoComplete="email"
+                    disabled={loading}
                     required
                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
