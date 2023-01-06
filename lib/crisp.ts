96db0c9a19abce3f945dee4a16393096c7dc7c73
Hasan <mr.k779@outlook.com>
2023-01-06 23:08:16 +0600

    Prisma client

--- lib/crisp.ts
+++ lib/crisp.ts
@@ -6,11 +6,11 @@ export const load = () => {
   Crisp.configure(process.env.NEXT_PUBLIC_CRSIP_WEBSITE_ID!);
 };
 
-export const configureUser = (user: User, subscripttion: Subscription) => {
+export const configureUser = (user: User, subscription: Subscription) => {
   Crisp.user.setEmail(user.email);
   Crisp.user.setNickname(user.name);
 
   Crisp.session.setData({
-    plan: subscripttion.product.name,
+    plan: subscription.product.name,
   });
 };
