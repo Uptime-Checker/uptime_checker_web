f926d593bd9506dd45af758fe90e7ad14d6945e0
Hasan <mr.k779@outlook.com>
2023-01-07 21:40:17 +0600

    Update topbar.tsx

--- components/dashboard/topbar.tsx
+++ components/dashboard/topbar.tsx
@@ -4,7 +4,8 @@ import { MagnifyingGlassIcon } from '@heroicons/react/20/solid';
 import { Bars3Icon, BellIcon } from '@heroicons/react/24/outline';
 import { useAtom } from 'jotai';
 import Link from 'next/link';
-import { Fragment, ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { Fragment, ReactNode, useEffect, useState } from 'react';
 import { globalAtom } from 'store/global';
 import { classNames } from 'utils/misc';
 
@@ -20,12 +21,42 @@ type Props = {
 };
 
 const TopBar = ({ className }: Props) => {
+  const router = useRouter();
   const [, setGlobal] = useAtom(globalAtom);
+  const [showSearch, setShowSearch] = useState(true);
+
+  useEffect(() => {
+    if (router.isReady) {
+      setShowSearch(router.pathname === '/monitors');
+    }
+  }, [router]);
 
   const toggleSidebar = () =>
     setGlobal((draft) => {
       draft.sidebar = !draft.sidebar;
     });
+
+  const searchBar = showSearch ? (
+    <div className="flex flex-1 pt-1 sm:pt-0">
+      <form className="flex w-full md:ml-0" action="#" method="GET">
+        <label htmlFor="search-field" className="sr-only">
+          Search
+        </label>
+        <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
+            <MagnifyingGlassIcon className="h-5 w-5" aria-hidden="true" />
+          </div>
+          <input
+            id="search-field"
+            className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
+            placeholder="Search"
+            type="search"
+            name="search"
+          />
+        </div>
+      </form>
+    </div>
+  ) : null;
 
   return (
     <div className={className}>
@@ -41,25 +72,7 @@ const TopBar = ({ className }: Props) => {
           </button>
         </div>
         <div className="flex flex-1 justify-end px-4 md:px-0">
-          <div className="flex flex-1 pt-1 sm:pt-0">
-            <form className="flex w-full md:ml-0" action="#" method="GET">
-              <label htmlFor="search-field" className="sr-only">
-                Search
-              </label>
-              <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                  <MagnifyingGlassIcon className="h-5 w-5" aria-hidden="true" />
-                </div>
-                <input
-                  id="search-field"
-                  className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
-                  placeholder="Search"
-                  type="search"
-                  name="search"
-                />
-              </div>
-            </form>
-          </div>
+          {searchBar}
           <section className="ml-4 flex items-center md:ml-6">
             <HeadwayWidget account={process.env.NEXT_PUBLIC_HEADWAY_WEBSITE_ID!}>
               <HeadwayWidgetTrigger>
