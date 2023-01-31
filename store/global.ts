0ae856d97524df1ef9d25ff2748401610c1f7f52
Hasan <mr.k779@outlook.com>
2023-02-01 00:57:52 +0600

    Update global.ts

--- store/global.ts
+++ store/global.ts
@@ -1,6 +1,7 @@
 import { atomWithImmer } from 'jotai-immer';
 import { OrganizationUser, User } from 'models/user';
 
+// This atom is used in the sidebar, so be careful to add more items to this type
 type GlobalAtom = {
   sidebar: boolean;
   currentUser: null | User;
