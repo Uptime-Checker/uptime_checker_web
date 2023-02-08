e2fc40eb82f2ea40e2e1800fc115307d0422a540
Hasan <mr.k779@outlook.com>
2023-02-09 01:53:52 +0600

    Update overview.tsx

--- pages/monitors/[monitorId]/overview.tsx
+++ pages/monitors/[monitorId]/overview.tsx
@@ -55,11 +55,11 @@ const Overview: NextPageWithLayout = () => {
       <section className="flex flex-col items-baseline justify-between lg:flex-row">
         <h2 className="mb-2 lg:mb-0 lg:pr-4">Response times across regions in the last day</h2>
         <div
-          className="flex items-baseline rounded bg-neutral-100 p-[2px]"
+          className="flex items-baseline rounded bg-neutral-100 p-[2px] text-sm font-medium text-gray-600"
           data-target="endpoints--response-times-tabs.tabs"
         >
           <div
-            className="cursor-pointer rounded bg-white px-4 py-1 text-neutral-800 shadow"
+            className="cursor-pointer rounded bg-white px-4 py-1 text-gray-800 shadow"
             data-action="click->endpoints--response-times-tabs#changeTab"
             data-preference="day"
             data-url="/team/44631/monitors/751810/response-chart?days=1"
