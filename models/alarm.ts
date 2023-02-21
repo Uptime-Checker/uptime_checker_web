650227c3b503e06b46e5d69f4e174df01b65ef8a
Hasan <mr.k779@outlook.com>
2023-02-22 02:57:21 +0600

    Update alarm.ts

--- models/alarm.ts
+++ models/alarm.ts
@@ -1,7 +1,10 @@
+import { Monitor } from './monitor';
+
 export interface Alarm {
   id: number;
   status: string;
   startedAt: string;
   resolvedAt: string;
   duration: string;
+  monitor?: Monitor;
 }
