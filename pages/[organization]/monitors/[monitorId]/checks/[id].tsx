5540ac21ca74f4dfb634622c1d2d8f46d2ca1593
Hasan <mr.k779@outlook.com>
2023-02-23 22:49:39 +0600

    Check detail

--- pages/[organization]/monitors/[monitorId]/checks/[id].tsx
+++ pages/[organization]/monitors/[monitorId]/checks/[id].tsx
@@ -1,12 +1,21 @@
-import { NextPageWithLayout } from '../../../../_app';
+import Link from 'next/link';
 import { ReactElement } from 'react';
 import DashboardLayout from '../../../../../layout/dashboard-layout';
+import { NextPageWithLayout } from '../../../../_app';
 
 const CheckDetail: NextPageWithLayout = () => {
   return (
     <section className="mx-auto mt-6 max-w-7xl px-4 sm:px-6 md:px-8">
       <div className="px-1 sm:px-6 md:px-0">
         <h1 className="text-2xl font-semibold text-gray-900">Check #47</h1>
+        <Link href="https://api.textrapp.me/api/v1/full_user_info" className="text-indigo-600 underline">
+          https://api.textrapp.me/api/v1/full_user_info
+        </Link>
+      </div>
+
+      <div className="flex flex-col">
+        <section></section>
+        <section></section>
       </div>
     </section>
   );
