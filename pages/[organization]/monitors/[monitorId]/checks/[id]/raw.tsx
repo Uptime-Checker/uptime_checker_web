3fedbea4642a6d28b3f62a94fc0f43dd172a6aff
Hasan <mr.k779@outlook.com>
2023-02-26 02:52:41 +0600

    Update raw.tsx

--- pages/[organization]/monitors/[monitorId]/checks/[id]/raw.tsx
+++ pages/[organization]/monitors/[monitorId]/checks/[id]/raw.tsx
@@ -3,8 +3,18 @@ import DashboardLayout from 'layout/dashboard-layout';
 import { NextPageWithLayout } from 'pages/_app';
 import { ReactElement } from 'react';
 
+const html = `<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>`;
+
 const ResponseRawView: NextPageWithLayout = () => {
-  return <h1>Raw</h1>;
+  return <pre className="mt-4 -mb-8 text-sm">{html}</pre>;
 };
 
 ResponseRawView.getLayout = function getLayout(page: ReactElement) {
