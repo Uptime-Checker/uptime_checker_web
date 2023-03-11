722617e87c21d0d21e9b644771f73483e70d1728
Hasan <mr.k779@outlook.com>
2023-03-12 04:46:54 +0600

    Update duration-bar.tsx

--- components/dashboard/monitor/duration-bar.tsx
+++ components/dashboard/monitor/duration-bar.tsx
@@ -1,26 +1,54 @@
+import { classNames } from 'lib/tailwind/utils';
 import { ResponseTimeKey } from 'models/monitor';
+import { useEffect, useState } from 'react';
 
 type Props = {
   responseTimes: Map<ResponseTimeKey, number>;
 };
+
+type ResponsePercentage = {
+  responseKey: ResponseTimeKey;
+  percentage: number;
+};
+
 const DurationBarComponent = ({ responseTimes }: Props) => {
+  const [responsePercentages, setResponsePercentages] = useState<ResponsePercentage[]>([]);
+
+  useEffect(() => {
+    let responsePercentages: ResponsePercentage[] = [];
+
+    let i = 0;
+    let totalTime = 0;
+    let totalPercentage = 0;
+
+    for (const [key, value] of responseTimes) {
+      i = i + 1;
+      if (key === ResponseTimeKey.TotalTime) {
+        totalTime = value;
+      } else {
+        let percentage = Math.round((value * 100) / totalTime);
+        totalPercentage = totalPercentage + percentage;
+
+        if (!Array.from(responseTimes.keys())[i] && totalPercentage < 100) {
+          percentage = percentage + 1;
+        }
+        let responsePercentage: ResponsePercentage = { responseKey: key, percentage: percentage };
+        responsePercentages.push(responsePercentage);
+      }
+    }
+
+    setResponsePercentages(responsePercentages);
+  }, [responseTimes]);
+
   const getBar = (responseTimeKey: ResponseTimeKey) => {
     if (responseTimeKey === ResponseTimeKey.TotalTime) return null;
 
-    let totalTime = 0;
-    let currentValue = 0;
-    for (const [key, value] of responseTimes) {
-      if (key === ResponseTimeKey.TotalTime) {
-        totalTime = value;
-      }
-      if (key === responseTimeKey) {
-        currentValue = value;
-      }
-    }
-    let percentage = Math.round((currentValue * 100) / totalTime);
-    if (percentage === 0) return null;
+    let percentage = responsePercentages.filter((responsePercentage) => {
+      return responsePercentage.responseKey === responseTimeKey;
+    })[0];
 
-    return <div key={responseTimeKey} className="w-1/4 bg-orange-500"></div>;
+    const width = `w-[${percentage}%]`;
+    return <div key={responseTimeKey} className={classNames(width, 'bg-red-500')}></div>;
   };
 
   return (
