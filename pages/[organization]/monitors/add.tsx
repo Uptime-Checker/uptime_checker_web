1d8ba6dea51dce17f8facf9fa39e72b23d135437
Hasan <mr.k779@outlook.com>
2023-03-24 17:40:54 +0600

    Monitor add sticky header

--- pages/[organization]/monitors/add.tsx
+++ pages/[organization]/monitors/add.tsx
@@ -5,11 +5,25 @@ import MonitorForm from 'components/dashboard/monitor/monitor-form';
 
 const MonitorAdd: NextPageWithLayout = () => {
   return (
-    <section className="mx-auto mt-6 max-w-7xl px-4 sm:px-6 md:px-8">
-      <div className="border-b px-1 pb-6 sm:px-6 md:px-0">
+    <section className="mx-auto max-w-7xl">
+      <div className="sticky top-0 flex items-center justify-between border-b bg-white bg-opacity-75 py-6 px-4 backdrop-blur backdrop-filter sm:px-6 md:px-8">
         <h1 className="text-2xl font-semibold text-gray-900">Add a new Monitor</h1>
+        <div className="flex gap-2">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 sm:w-auto"
+          >
+            Run Before Saving
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+          >
+            Save
+          </button>
+        </div>
       </div>
-      <div className="mt-6">
+      <div className="mt-6 px-4 sm:px-6 md:px-8">
         <MonitorForm />
       </div>
     </section>
