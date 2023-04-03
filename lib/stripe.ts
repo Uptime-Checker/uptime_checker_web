cc05e9d83515aa256d3b2f79833e83c953488d5f
Hasan <mr.k779@outlook.com>
2023-04-03 22:37:02 +0600

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,9 +2,9 @@ import { Stripe, loadStripe } from '@stripe/stripe-js';
 
 let stripePromise: Promise<Stripe | null>;
 
-const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const getStripe = async () => {
+  if (!(await stripePromise)) {
+    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
   }
   return stripePromise;
 };
