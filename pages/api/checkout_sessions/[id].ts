cd6adc1e158a8fcec10273000dc76fc9578fa26f
Hasan <mr.k779@outlook.com>
2023-05-07 13:28:41 +0600

    Update [id].ts

--- pages/api/checkout_sessions/[id].ts
+++ pages/api/checkout_sessions/[id].ts
@@ -1,3 +1,4 @@
+import { HttpStatusCode } from 'axios';
 import { STRIPE_API_VERSION } from 'constants/default';
 import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
@@ -10,7 +11,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const id = String(req.query.id);
   try {
     if (!id.startsWith('cs_')) {
-      throw Error('Incorrect CheckoutSession ID');
+      res.status(HttpStatusCode.InternalServerError).json({ message: 'Incorrect CheckoutSession ID' });
+      return;
     }
     const checkout_session = await stripe.checkout.sessions.retrieve(id, {
       expand: ['payment_intent'],
@@ -19,6 +21,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.status(200).json(checkout_session);
   } catch (err) {
     const errorMessage = err instanceof Error ? err.message : 'Internal server error';
-    res.status(500).json({ statusCode: 500, message: errorMessage });
+    res.status(HttpStatusCode.InternalServerError).json({ message: errorMessage });
   }
 }
