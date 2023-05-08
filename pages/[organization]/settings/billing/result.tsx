0cbf47ec070682984ab5cbb9198c6e90c7847af1
Hasan <mr.k779@outlook.com>
2023-05-08 22:03:00 +0600

    Update result.tsx

--- pages/[organization]/settings/billing/result.tsx
+++ pages/[organization]/settings/billing/result.tsx
@@ -1,3 +1,5 @@
+import { ExclamationCircleIcon } from '@heroicons/react/24/solid';
+import * as Sentry from '@sentry/nextjs';
 import axios from 'axios';
 import LoadingBubbleIcon from 'components/icon/loading-bubble';
 import SuccessIcon from 'components/icon/success';
@@ -34,25 +36,49 @@ const Result: NextPageWithLayout = () => {
           })
         );
       })
-      .catch((err) => {});
+      .catch((err) => {
+        setUIState(
+          produce((draft) => {
+            draft.loading = false;
+            draft.success = false;
+          })
+        );
+        Sentry.captureException(err);
+      });
   }, [router.isReady, router.query]);
 
-  const getTitle = () => {};
+  const getTitle = () => {
+    return uiState.success ? 'Payment Successful!' : 'Payment Failed!';
+  };
+
+  const getDescription = () => {
+    return uiState.success
+      ? 'Thank you for completing your secure online payment.'
+      : 'We failed to charge you. Please try again. If you need assistance, please contact support.';
+  };
+
+  const getBackURL = () => {
+    return uiState.success ? `/${orgSlug!}/monitors` : `/${orgSlug!}/settings/billing`;
+  };
 
   return (
     <div className="p-6 md:mx-auto">
       {uiState.loading ? <LoadingBubbleIcon className="mx-auto mt-4 w-32" /> : null}
       {uiState.loading ? null : (
         <section>
-          <SuccessIcon className="mx-auto my-6 h-16 w-16 text-green-600" />
+          {uiState.success ? (
+            <SuccessIcon className="mx-auto my-6 h-16 w-16 text-green-600" />
+          ) : (
+            <ExclamationCircleIcon className="mx-auto my-6 h-16 w-16 text-rose-500" />
+          )}
           <div className="text-center">
-            <h3 className="text-center text-base font-semibold text-gray-900 md:text-2xl">Payment Done!</h3>
-            <p className="my-2 text-gray-600">Thank you for completing your secure online payment.</p>
-            <p> Have a great day! </p>
+            <h3 className="text-center text-base font-semibold text-gray-900 md:text-2xl">{getTitle()}</h3>
+            <p className="my-2 text-gray-600">{getDescription()}</p>
+            {uiState.success ? <p> Have a great day! </p> : null}
             <div className="py-10 text-center">
               <Link
                 type="button"
-                href={`/${orgSlug!}/monitors`}
+                href={getBackURL()}
                 className="rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
               >
                 Go Back
