cba2910401bc2f81ab9ef76d5937c5261341d493
Hasan <mr.k779@outlook.com>
2023-05-09 20:05:05 +0600

    Update monitor-form.tsx

--- components/dashboard/monitor/monitor-form.tsx
+++ components/dashboard/monitor/monitor-form.tsx
@@ -46,12 +46,12 @@ const MonitorFormComponent = () => {
               </div>
             </div>
             <div className="sm:col-span-3">
-              <label htmlFor="location" className="block text-sm font-medium leading-6 text-gray-900">
-                Location
+              <label htmlFor="frequency" className="block text-sm font-medium leading-6 text-gray-900">
+                Frequency
               </label>
               <select
-                id="location"
-                name="location"
+                id="frequency"
+                name="frequency"
                 className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue="Every 3 Minutes"
               >
