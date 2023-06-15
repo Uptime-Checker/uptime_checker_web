8bd3e2a95e2e0692fcf36ffc381f138c702893dd
Hasan <mr.k779@outlook.com>
2023-06-15 18:26:29 +0600

    Update monitor-form.tsx

--- components/dashboard/monitor/monitor-form.tsx
+++ components/dashboard/monitor/monitor-form.tsx
@@ -187,7 +187,6 @@ const MonitorFormComponent = () => {
                   <label htmlFor="tabs" className="sr-only">
                     Select a tab
                   </label>
-                  {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
                   <select
                     id="tabs"
                     name="tabs"
