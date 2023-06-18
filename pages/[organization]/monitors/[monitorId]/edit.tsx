b219d94c65830f33b31e3a23842869ec3491a508
Rafi <mr.k779@outlook.com>
2023-06-18 16:42:23 +0600

    Monitor edit

---
+++ pages/[organization]/monitors/[monitorId]/edit.tsx
@@ -0,0 +1,59 @@
+import * as Sentry from '@sentry/nextjs';
+import MonitorForm from 'components/dashboard/monitor/monitor-form/monitor-form';
+import DashboardLayout from 'layout/dashboard-layout';
+import { HTTPMethod, authRequest } from 'lib/axios';
+import { MonitorResponse } from 'models/monitor';
+import { useRouter } from 'next/router';
+import { NextPageWithLayout } from 'pages/_app';
+import { ReactElement, useEffect } from 'react';
+
+const MonitorAdd: NextPageWithLayout = () => {
+  const router = useRouter();
+  // const [monitor, setMonitor] = useState<Monitor>(null);
+
+  useEffect(() => {
+    if (router.isReady) {
+      authRequest<MonitorResponse>({
+        method: HTTPMethod.GET,
+        url: `/monitor/${router.query.monitorId as string}`,
+      })
+        .then((resp) => {
+          console.log(resp.data.data);
+        })
+        .catch((e) => {
+          Sentry.captureException(e);
+        });
+    }
+  }, [router]);
+
+  return (
+    <section className="mx-auto max-w-7xl">
+      <div className="sticky top-0 z-10 flex items-center justify-between border-b bg-white bg-opacity-75 px-4 py-6 backdrop-blur backdrop-filter sm:px-6 md:px-8">
+        <h1 className="text-2xl font-semibold text-gray-900">Add a new Monitor</h1>
+        <div className="flex gap-2">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 sm:w-auto"
+          >
+            Test Run
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+          >
+            Update
+          </button>
+        </div>
+      </div>
+      <div className="mt-6 px-4 sm:px-6 md:px-8">
+        <MonitorForm />
+      </div>
+    </section>
+  );
+};
+
+MonitorAdd.getLayout = function getLayout(page: ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
+
+export default MonitorAdd;
