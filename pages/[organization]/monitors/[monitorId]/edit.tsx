51252eec4416e125ce4f6a5dde8f10874da1f4f8
Rafi <mr.k779@outlook.com>
2023-06-21 04:09:40 +0600

    Update edit.tsx

--- pages/[organization]/monitors/[monitorId]/edit.tsx
+++ pages/[organization]/monitors/[monitorId]/edit.tsx
@@ -54,7 +54,8 @@ const MonitorAdd: NextPageWithLayout = () => {
                 Test Run
               </button>
               <button
-                type="button"
+                type="submit"
+                form="monitorForm"
                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
               >
                 Update
