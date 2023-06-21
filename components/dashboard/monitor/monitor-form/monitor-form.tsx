f5a36a996009819c5a8e40ccc0cf00a7f8363b2c
Rafi <mr.k779@outlook.com>
2023-06-22 02:06:13 +0600

    Update monitor-form.tsx

--- components/dashboard/monitor/monitor-form/monitor-form.tsx
+++ components/dashboard/monitor/monitor-form/monitor-form.tsx
@@ -214,9 +214,7 @@ const MonitorFormComponent = () => {
                   </label>
                   <div className="mt-2">
                     <select
-                      id="method"
                       name="method"
-                      autoComplete="method-name"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                     >
                       <option>GET</option>
@@ -225,6 +223,23 @@ const MonitorFormComponent = () => {
                       <option>HEAD</option>
                       <option>PATCH</option>
                       <option>DELETE</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label htmlFor="method" className="block text-sm font-medium leading-6 text-gray-900">
+                    Timeout
+                  </label>
+                  <div className="mt-2">
+                    <select
+                      name="timeout"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    >
+                      <option>5 seconds</option>
+                      <option>10 seconds</option>
+                      <option>30 seconds</option>
+                      <option>60 seconds</option>
                     </select>
                   </div>
                 </div>
