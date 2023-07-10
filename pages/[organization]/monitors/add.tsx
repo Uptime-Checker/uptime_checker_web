0b900d9b0397e417da928c8ceaaadef693bdf836
Hasan <mr.k779@outlook.com>
2023-07-10 14:22:44 +0600

    Update add.tsx

--- pages/[organization]/monitors/add.tsx
+++ pages/[organization]/monitors/add.tsx
@@ -1,16 +1,35 @@
 import MonitorForm from 'components/dashboard/monitor/monitor-form/monitor-form';
 import DashboardLayout from 'layout/dashboard-layout';
+import { Assertion } from 'models/assertion';
+import { Monitor } from 'models/monitor';
 import { NextPageWithLayout } from 'pages/_app';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 
 const MonitorAdd: NextPageWithLayout = () => {
+  const [dryRun, setDryRun] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      setDryRun(false);
+    };
+  }, []);
+
+  const handleSubmit = (monitor: Monitor, assertions: Assertion[]) => {
+    console.log(monitor);
+    console.log(assertions);
+
+    // handle dry run or submit
+  };
+
   return (
     <section className="mx-auto max-w-7xl">
       <div className="sticky top-0 z-10 flex items-center justify-between border-b bg-white bg-opacity-75 px-4 py-6 backdrop-blur backdrop-filter sm:px-6 md:px-8">
         <h1 className="text-2xl font-semibold text-gray-900">Add a new Monitor</h1>
         <div className="flex gap-2">
           <button
-            type="button"
+            type="submit"
+            form="monitorForm"
+            onClick={() => setDryRun(true)}
             className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 sm:w-auto"
           >
             Test Run
@@ -18,6 +37,7 @@ const MonitorAdd: NextPageWithLayout = () => {
           <button
             type="submit"
             form="monitorForm"
+            onClick={() => setDryRun(false)}
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
           >
             Save
